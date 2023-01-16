5c3e3e58d06c05de7ae911003ebc944e053869c7
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-16 22:04:08 +0900

    create Blog post lists page using FBV

--- project/doitsite/blog/views.py
+++ project/doitsite/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk') #모든 Post 레코드 가져오기 / pk를 역순으로 정렬 (최신순)
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
