543f10c76ee038a6c8124235ca54ff29dbd32417
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-16 18:32:09 +0900

    create Post model and Time setting

--- project/doitsite/blog/models.py
+++ project/doitsite/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30) #제목
+    content = models.TextField() #내용
+
+    created_at = models.DateTimeField(auto_now_add=True) #작성일
+    updated_at = models.DateTimeField(auto_now=True)
+    # author
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
