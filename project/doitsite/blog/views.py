97d39ea668c6768563ead70324f54b0b06546bd7
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-17 00:16:03 +0900

    create Blog Post List page using CBV

--- project/doitsite/blog/views.py
+++ project/doitsite/blog/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk') #모든 Post 레코드 가져오기 / pk를 역순으로 정렬 (최신순)
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk) #single_post_page() 함수의 매개변수로 받은 pk와 Post 모델의 pk 필드 값이 같은 레코드 가져오기
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk') #모든 Post 레코드 가져오기 / pk를 역순으로 정렬 (최신순)
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+#
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk) #single_post_page() 함수의 매개변수로 받은 pk와 Post 모델의 pk 필드 값이 같은 레코드 가져오기
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post': post,
+#         }
+#     )
