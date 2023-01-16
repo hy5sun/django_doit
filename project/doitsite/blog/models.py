6509f951ce30c616dc303b54f4c809bacbc6adfa
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-17 02:44:32 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- project/doitsite/blog/models.py
+++ project/doitsite/blog/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30) #제목
     content = models.TextField() #내용
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #서로 다른 위치에 저장해야 시간 단축 가능 / blank=True는 필수 항목이 아니라는 뜻
     created_at = models.DateTimeField(auto_now_add=True) #작성일
     updated_at = models.DateTimeField(auto_now=True)
     # author
--- project/doitsite/do_it_django_prj/settings.py
+++ project/doitsite/do_it_django_prj/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +120,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
