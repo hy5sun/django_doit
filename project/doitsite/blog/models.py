f0830fb14504b027c373fbf1c546c8fb3739a710
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-17 02:51:55 +0900

    포스트 모델에 file_upload 필드 추가하기

--- project/doitsite/blog/models.py
+++ project/doitsite/blog/models.py
@@ -5,6 +5,8 @@ class Post(models.Model):
     content = models.TextField() #내용
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #서로 다른 위치에 저장해야 시간 단축 가능 / blank=True는 필수 항목이 아니라는 뜻
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True) #작성일
     updated_at = models.DateTimeField(auto_now=True)
     # author
