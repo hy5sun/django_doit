9958032b52012313ebd75b6cbe70f58504de2c84
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-17 17:55:20 +0900

    포스트 미리보기 & 요약문 추가하기

--- project/doitsite/blog/models.py
+++ project/doitsite/blog/models.py
@@ -3,6 +3,7 @@ import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30) #제목
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField() #내용
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True) #서로 다른 위치에 저장해야 시간 단축 가능 / blank=True는 필수 항목이 아니라는 뜻
