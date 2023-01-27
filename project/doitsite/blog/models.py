a6792cc36f62d79f99f5eac27306735fc780cee3
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-01-27 23:28:43 +0900

    create category page

--- project/doitsite/blog/models.py
+++ project/doitsite/blog/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
     
     class Meta: #복수형 직접 입력
         verbose_name_plural = 'Categories'
--- project/doitsite/blog/views.py
+++ project/doitsite/blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
@@ -8,8 +9,27 @@ class PostList(ListView):
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
-        context['np_category_post_count'] = Post.objects.filter(category=None).count()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류',
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(), # 페이지의 오른쪽에 위치한 카테고리 카드를 채워준다.
+            'no_category_post_count': Post.objects.filter(category=None).count(), # 카테고리 카드 맨 아래에 미분류 포스트와 개수를 알려준다.
+            'category': category # 페이지 타이틀 옆에 카테고리 이름을 알려준다.
+        }
+    )
 
 class PostDetail(DetailView):
     model = Post
