fa5cde1ebfe0fabb3653511e5885da9932d07d77
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-02-03 00:52:39 +0900

    fix typo

--- project/doitsite/blog/admin.py
+++ project/doitsite/blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from markdownx.admin import MarkdownModelAdmin
+from markdownx.admin import MarkdownxModelAdmin
 from .models import Post, Category, Tag
 
-admin.site.register(Post, MarkdownModelAdmin)
+admin.site.register(Post, MarkdownxModelAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )} # Category 모델의 name 필드에 값이 입력되면 자동으로 slug가 만들어진다.
